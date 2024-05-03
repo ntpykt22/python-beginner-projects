22dcdf96184d0eb8917641ac82f342768439b832
Nuttarpon <nuttapon.ykt@gmail.com>
2024-05-03 12:02:10 +0700

    [Updated] เพิ่มวิธีการใช้ String Methods

--- Day01/Lesson04/data_type.py
+++ Day01/Lesson04/data_type.py
@@ -44,3 +44,21 @@ print(multiline)
 # Escaping special characters
 sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\\located?'
 print(sentence)
+
+# String Methods
+
+print(first)
+print(first.lower())
+print(first.upper())
+print(first)
+
+print(multiline.title())
+print(multiline.replace("good", "ok"))
+print(multiline)
+
+print(len(multiline))
+multiline += "                    "
+multiline = "              " + multiline
+print(len(multiline))
+
+
