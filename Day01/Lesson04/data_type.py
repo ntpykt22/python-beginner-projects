1e79d9a29135b33222da16f7705b9c3d6c417d7b
Nuttarpon <nuttapon.ykt@gmail.com>
2024-05-03 11:42:35 +0700

    [Updated] เพิ่มการใช้ Concatenation

--- Day01/Lesson04/data_type.py
+++ Day01/Lesson04/data_type.py
@@ -5,6 +5,21 @@ first = 'Nuttarpoz'
 last = 'Yakathurm'
 
 #แสดง first เท่ากันกับ ประเภท str
-print(type(first))
-print(type(first) == str)
-print(isinstance(first, str))
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+# consturctor function
+# pizza = str("Peperoni")
+# # Press "Command + D" for เปลี่ยนค่าตัวแปรครั้งเดียว หลายๆ บรรทัดให้เหมือนกัน
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza , str))
+
+# Concatenation
+fullname = first + " " + last
+print(fullname)
+
+fullname += "!"
+print(fullname)
+
