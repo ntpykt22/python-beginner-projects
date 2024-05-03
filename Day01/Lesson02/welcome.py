95e2760ec9885d50a2ff4c299f0f8947f9ac9714
Nuttarpon <nuttapon.ykt@gmail.com>
2024-05-03 10:50:30 +0700

    [Updated] การประกาศตัวแปร และ ลักษณะการใช้ print()

--- Day01/Lesson02/welcome.py
+++ Day01/Lesson02/welcome.py
@@ -1,8 +1,9 @@
-#Press "Shift + Option + DownArrow" for Quick Duplicate  
-line01 = "*********************"
-line02 = "*                   *"
+# Press "Shift + Option + DownArrow" for Quick Duplicate  
+line01 = "*********************" # header / footer
+line02 = "*                   *" # re-use
 line03 = "*      Welcome!     *"
 
+# start with a blank line 
 print('')
 print(line01)
 print(line02)
