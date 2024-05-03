95a10348516438bdbbaba2498f6759199eb713e7
Nuttarpon <nuttapon.ykt@gmail.com>
2024-05-03 11:33:46 +0700

    [Updated] เพิ่ม check variable type

---
+++ Day01/Lesson04/data_type.py
@@ -0,0 +1,10 @@
+# String Data type
+
+#literal assignment
+first = 'Nuttarpoz'
+last = 'Yakathurm'
+
+#แสดง first เท่ากันกับ ประเภท str
+print(type(first))
+print(type(first) == str)
+print(isinstance(first, str))
