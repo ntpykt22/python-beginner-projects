9c9488cd88719d8ec1d65c23e3916d85d8773520
Nuttarpon <nuttapon.ykt@gmail.com>
2024-05-03 11:26:01 +0700

    [Updated] ฝึกการใช้ if else และ วิธีการเขียน

---
+++ Day01/Lesson03/meaning.py
@@ -0,0 +1,12 @@
+meaning = 48
+print('')
+
+# if meaning > 10:
+#     print('Right On!!!')
+# else:
+#     print('Not today!!!')
+
+# Ternary Operator
+print('Right On!!!') if meaning > 10 else print('Not today!!!')
+
+#แสดงให้เห็นวิธีการที่ดีกว่า และวิธีการเขียนที่แตกต่างกัน
