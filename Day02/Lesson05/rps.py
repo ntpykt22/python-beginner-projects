a50bae825ad2744393ce4965a85951eca88cb987
Nuttarpon <nuttapon.ykt@gmail.com>
2024-05-15 14:15:09 +0700

    [Updated] เพิ่ม ระบบเกมเป่ายิ้งฉุบ

---
+++ Day02/Lesson05/rps.py
@@ -0,0 +1,47 @@
+# value = input('Please enter a value:\n')
+# print(value)
+
+import sys
+import random
+from enum import Enum
+
+class RPS(Enum):
+   ROCK = 1
+   PAPER = 2
+   SCISSORS = 3
+
+# print(RPS(2))
+# print(RPS.ROCK)
+# print(RPS['ROCK'])
+# print(RPS.ROCK.value)
+# sys.exit()
+
+print("")
+playerchoice = input("Enter... \n1 for Rock, \n2 for Paper, or \n3 for Scissirs: \n\n")
+
+player = int(playerchoice)
+
+# if player < 1 | player > 3: reccomend เปลี่ยนไปใช้แบบล่าง
+if player < 1 or player > 3:
+   sys.exit("You must enter 1, 2, or 3.")
+
+computchoice = random.choice("123")
+
+computer = int(computchoice)
+
+print("")
+print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+print("")
+
+if player == 1 and computer == 3: 
+   print("You win!")
+elif player == 2 and computer == 1: 
+   print("You win!")
+elif player == 3 and computer == 2: 
+   print("You win!")
+elif player == computer: 
+   print("Tie game!")
+else:
+   print("Python wins!")
+
